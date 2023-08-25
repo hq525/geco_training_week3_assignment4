bf3adf51f79695f03eafbac606d51029642ee404
hq525 <zhaohanqing96@gmail.com>
2023-08-25 18:26:52 +0800

    feat: answer submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  var [pets] = useState([
+    {
+      species: "cat",
+      breed: "ragdoll",
+      color: "white"
+    },
+    {
+      species: "dog",
+      breed: "pug",
+      color: "white"
+    },
+    {
+      species: "rabbit",
+      breed: "havana",
+      color: "white"
+    }
+  ])
+
+  var [obj, setObj] = useState({
+    randNum: Math.random(),
+    currTime : new Date().toTimeString().split(" ")[0]
+  })
+
+  var updateObj = () => {
+    setObj({
+      randNum: Math.random(),
+      currTime : new Date().toTimeString().split(" ")[0]
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+      <h1>Pets</h1>
+      {pets.map((pet) => {
+        return(
+          <div style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
+            Species: {pet.species}
+            <br />
+            Breed: {pet.breed}
+            <br />
+            Color: {pet.color}
+          </div>
+        )
+      })}
+      <h1>Object</h1>
+      <div style={{ border: "1px solid black", width: "fit-content", margin: "10px", padding: "10px", display: "block", marginLeft: "auto", marginRight: "auto" }}>
+        Random Number: {obj.randNum}
+        <br />
+        Current Time: {obj.currTime}
+        <br />
+        <button onClick={updateObj}>Update</button>
+      </div>
     </div>
   );
 }
